61010e232f01319895b8f8b22693d49e4073e892
RodrigoDiazCarrillo <dzcll90@gmail.com>
2022-05-27 16:36:28 +0200

    styles ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,40 @@
 
 import React, { useContext } from 'react';
 import ThemeContext from '../context';
-//import './Header.css'
-//import { BsFillMoonStarsFill} from "react-icons/bs";
-//import { FaSun} from "react-icons/fa";
-//import { useTheme, Theme } from '../ThemeContext';
+import './Header.css'
+import { BsFillMoonStarsFill} from "react-icons/bs";
+import { FaSun} from "react-icons/fa";
+import {Social_bar } from './Social_bar';
 
 export function Component_header(){
   const { theme, handleChangeTheme } = useContext(ThemeContext);
   console.log("tema"+theme.name);
+  var text = <FaSun/>;
+  if (theme.name=="Light") {text=<BsFillMoonStarsFill/>;}
+  else {text=<FaSun/>}
   return (
     <div className="header_main">
         <div id={"nav_bar"+theme.name}>
-          
-          </div>
-        <button
-        onClick={handleChangeTheme}
-        style={{ background: theme.background, color: theme.foreground }}>
-        Change Theme
+        <a>R</a>
+          <a>o</a>
+          <a>d</a>
+          <a>r</a>
+          <a>i</a>
+          <a>g</a>
+          <a>0</a>
+          <a><br/> </a>
+          <a>d</a>
+          <a>i</a>
+          <a>a</a>
+          <a>z</a>
+
+          <Social_bar />
+        <button id={'themeBtn'+theme.name}
+        onClick={handleChangeTheme}>
+        {text}
         </button>
+          </div>
+        
     </div>
   );
 }
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,18 +1,19 @@
 import './Presentation.css'
 import { BsFillMoonStarsFill} from "react-icons/bs";
 import { FaSun} from "react-icons/fa";
-//import { useTheme, Theme } from '../ThemeContext';
+import ThemeContext from '../context';
+import React, { useContext } from 'react';
 
 export function Presentation() {
-  //const { theme, setTheme } = useTheme();
+  const {theme} = useContext(ThemeContext);
   // var text = <FaSun/>;
   // if (theme=="Light") {text=<BsFillMoonStarsFill/>;}
   // else {text=<FaSun/>}
   
   return (
-        //console.log(theme),
+
         <div>
-        <div className={"presentation"}>
+        <div className={"presentation"+theme.name}>
         <p><a><strong>Hello!</strong> My name is </a>
         <a><strong>Rodrigo</strong>,</a>
         <a>I'm junior Web Developer</a></p>
