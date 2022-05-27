6e967fee98771a9303a1ade74cfe7ad1b9f0684a
RodrigoDiazCarrillo <dzcll90@gmail.com>
2022-05-27 15:51:49 +0200

    background included

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default function App() {
   const [theme, setTheme] = useState(themes.light);
 
   const handleChangeTheme = () => {
+
     setTheme(() => {
       return theme === themes.dark ? themes.light : themes.dark;
     });
@@ -20,40 +21,43 @@ export default function App() {
 
   return (
     <ThemeContext.Provider value={{ theme, handleChangeTheme }}>
-
-        <div className="App">
-        </div>
-        <div id="Layout" style={{ background: theme.background, color: theme.foreground }}>
+      
+           
+    <div id={"main"} className={"app"+theme.name}>     
             <Component_header/>
             <Presentation/>   
             <About/>
             <Skills/>
             <CV/>
             <Projects/>
-            <Appa/>
-
-
+            <div className={"appa"}>
+                <Appa/>
+            </div>
     </div>
-  
     </ThemeContext.Provider>
   );
 }
 
-
+let w=window.innerWidth;
+let h= document.querySelector("html").scrollHeight;
+let button=true;
+//document.getElementById("main").className
 
 class Appa extends React.Component {
   
-	setup = (p5, parentRef) => {
-
-
-		p5.createCanvas(500,500).parent(parentRef);
+	setup = (p5, main) => {
+    let themeValue =document.getElementById("main").className;
+		p5.createCanvas(window.innerWidth,window.innerHeight).parent(main);
     p5.background(0,180,180);
     p5.strokeWeight(1);
     p5.stroke(0);
     for(let i=0;i<5000;i++){
-
+      if (themeValue==="appLight"){
         p5.fill(0,p5.random(150,250),p5.random(150,250));
-
+      }
+      if (themeValue==="appDark"){
+        p5.fill(p5.random(0,250),0,0);
+      }
       p5.stroke(0);
       let x= p5.random(0,window.innerWidth);
       let y= p5.random(0,window.innerHeight);
@@ -63,6 +67,69 @@ class Appa extends React.Component {
     
 
  }
+ draw =(p5,main)=>{
+  
+  let themeValue =document.getElementById("main").className;
+  
+  if (themeValue=="appLight"){
+   p5.fill(0,p5.random(150,250),p5.random(150,250));
+ }
+ if (themeValue=="appDark"){
+   p5.fill(p5.random(0,150),0,0);
+ }
+ if (themeValue=="appDark" && button==true){
+   dark();
+ }
+ if (themeValue=="appLight" && button==false){
+   light();
+ }
+  if (w!=window.innerWidth || h!= window.innerHeight){
+   p5.createCanvas(window.innerWidth, window.innerHeight).parent(main);
+   w=window.innerWidth;
+   h= window.innerHeight;
+   switch(themeValue){
+     case "appDark": dark();
+     break;
+     case "appLight": light();
+     break;
+   }
+  } 
+ 
+
+ p5.stroke(0);
+ let x= p5.random(0,window.innerWidth);
+ let y= p5.random(0,window.innerHeight);
+ p5.ellipse(x,y,15,15);
+
+ function dark (){
+   console.log("click");
+   button=false;
+   if(themeValue=="appDark"){
+     p5.background(100,0,0);
+   for(let i=0;i<5000;i++){
+     p5.fill(p5.random(0,250),0,0);
+     p5.stroke(0);
+     let x= p5.random(0,window.innerWidth);
+     let y= p5.random(0,window.innerHeight);
+     p5.ellipse(x,y,15,15);
+   }
+ }
+}
+function light (){
+ console.log("click");
+ button=true;
+ if(themeValue=="appLight"){
+   p5.background(0,180,180);
+ for(let i=0;i<5000;i++){
+   p5.fill(0,p5.random(150,250),p5.random(150,250));
+   p5.stroke(0);
+   let x= p5.random(0,window.innerWidth);
+   let y= p5.random(0,window.innerHeight);
+   p5.ellipse(x,y,15,15);
+ }
+}
+}
+}
 
  
  
