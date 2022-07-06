0c403054bb9c78d7e2bff7b04c4c85909689d163
RodrigoDiazCarrillo <dzcll90@gmail.com>
2022-07-06 16:42:27 +0200

    responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import './Projects.css'
 import web from '../img/web1.jpg';
+import pr1 from '../img/project_1.jpg';
+import pr2 from '../img/portfolio.jpg';
 import ThemeContext from '../context';
 import React, { useContext } from 'react';
 
@@ -11,10 +13,21 @@ export function Projects() {
         <p className="title">Projects</p>
 
         <div className='pr'>
-        <img src={web} ></img>
-        <img src={web} ></img>
-        <img src={web} ></img>
-        <img src={web} ></img>
+        <div>
+        <p>My portfolio<br/> (React)</p>
+        <a href='https://github.com/RodrigoDiazCarrillo/RodrigoDiaz.git' target="_blank">
+          <img src={pr2} ></img></a>
+        </div>
+        <div>
+        <p>Web app<br/> (React)</p>
+        <a href='https://github.com/RodrigoDiazCarrillo/bootcamp-project-frontend.git' target="_blank">
+          <img src={pr1} ></img></a>
+        </div>
+        <div>
+        <p>Restful API (NodeJS)</p>
+        <a href='https://github.com/RodrigoDiazCarrillo/bootcamp-project-backend.git' target="_blank">
+          <img src={web} ></img></a>
+        </div>
         </div>
       </div>
     );
--- src/components/Social_bar.js
+++ src/components/Social_bar.js
@@ -5,7 +5,7 @@ import './Social_bar.css'
 export function Social_bar() {
     return (
       <div className="icons">
-        <a href="https://www.linkedin.com/in/rodrigo-d%C3%ADaz-carrillo-0203741b5/" target="_blank"><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/rodrigo-diaz-carrillo/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/RodrigoDiazCarrillo" target="_blank"><BsGithub/></a>
         <a href="mailto:dzcll90@gmail.com"><MdEmail/></a>
       </div>
